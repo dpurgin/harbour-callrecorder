53f91c85f4d053649d7adc93356799bb26e52c2c
Dmitriy Purgin <dpurgin@gmail.com>
2015-04-13 23:29:07 +0600

    [ui] german translation

---
+++ ui/translations/ui-de.ts
@@ -0,0 +1,399 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
+<context>
+    <name>About</name>
+    <message>
+        <source>About</source>
+        <translation>Über</translation>
+    </message>
+    <message>
+        <source>Call Recorder</source>
+        <translation>Call Recorder</translation>
+    </message>
+    <message>
+        <source>for SailfishOS</source>
+        <translation>für SailfishOS</translation>
+    </message>
+    <message>
+        <source>Copyright © 2014-2015 Dmitriy Purgin</source>
+        <translation>Copyright © 2014-2015 Dmitriy Purgin</translation>
+    </message>
+    <message>
+        <source>Thanks to Simonas Leleiva and Juho Hämäläinen</source>
+        <translation>Danke an Simonas Leleiva und Juho Hämäläinen</translation>
+    </message>
+    <message>
+        <source>This program comes with ABSOLUTELY NO WARRANTY. This is free software, and you are welcome to redistribute it under certain conditions; click on the button below for details</source>
+        <translation>Das Programm kommt mit KEINERLEI GARANTIE. Dies ist freie Software, und Sie sind herzlich eingeladen, um sie unter bestimmten Bedingungen weitergeben dürfen; klicken Sie auf die Schaltfläche unten für Details</translation>
+    </message>
+    <message>
+        <source>Show license</source>
+        <translation>Lizenz anzeigen</translation>
+    </message>
+    <message>
+        <source>Version %1</source>
+        <translation>Version %1</translation>
+    </message>
+</context>
+<context>
+    <name>AudioSettings</name>
+    <message>
+        <source>Audio Settings</source>
+        <translation>Audio Einstellungen</translation>
+    </message>
+    <message>
+        <source>FLAC Encoder</source>
+        <translation>FLAC Encoder</translation>
+    </message>
+    <message>
+        <source>Sample rate</source>
+        <translation>Abtastrate</translation>
+    </message>
+    <message>
+        <source>FLAC compression level</source>
+        <translation>FLAC Komprimierungsstufe</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Call Recorder</source>
+        <translation>Call Recorder</translation>
+    </message>
+</context>
+<context>
+    <name>Daemon</name>
+    <message>
+        <source>Recording daemon</source>
+        <translation>Aufnahme-Daemon</translation>
+    </message>
+    <message>
+        <source>Startup</source>
+        <translation>Autostart</translation>
+    </message>
+    <message>
+        <source>Active</source>
+        <translation>Aktiv</translation>
+    </message>
+    <message>
+        <source>Capture all incoming and outgoing calls</source>
+        <translation>Erfassen Sie alle ein- und ausgehende Anrufe</translation>
+    </message>
+    <message>
+        <source>Automatic startup</source>
+        <translation>Automatischer Start</translation>
+    </message>
+    <message>
+        <source>Start automatically upon reboot</source>
+        <translation>Automatisch starten beim Neustart</translation>
+    </message>
+    <message>
+        <source>Operation mode</source>
+        <translation>Betriebsart</translation>
+    </message>
+    <message>
+        <source>Record calls to any number except those in the black list</source>
+        <translation>Alle Anrufe annehmen einer beliebigen Anzahl mit Ausnahme der in der schwarzen Liste</translation>
+    </message>
+    <message>
+        <source>Do not record anything except numbers in the white list</source>
+        <translation>Nichts aufnehmen außer die Zahlen in der weißen Liste</translation>
+    </message>
+    <message>
+        <source>Black list</source>
+        <translation>Schwarze Liste</translation>
+    </message>
+    <message>
+        <source>White list</source>
+        <translation>Weiße Liste</translation>
+    </message>
+    <message>
+        <source>Edit list</source>
+        <translation>Liste bearbeiten</translation>
+    </message>
+</context>
+<context>
+    <name>DirectoryNameDialog</name>
+    <message>
+        <source>Directory name</source>
+        <translation>Verzeichnisname</translation>
+    </message>
+</context>
+<context>
+    <name>DirectoryPickerDialog</name>
+    <message>
+        <source>Select %1</source>
+        <translation>Auswählen  %1</translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation>Auswählen</translation>
+    </message>
+    <message>
+        <source>No nested directories</source>
+        <translation>Keine verschachtelten Verzeichnisse</translation>
+    </message>
+    <message>
+        <source>Rename</source>
+        <translation>Umbenennen</translation>
+    </message>
+    <message>
+        <source>Renaming is not allowed</source>
+        <translation>Umbenennen ist nicht erlaubt</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Löschen</translation>
+    </message>
+    <message>
+        <source>Removal is not allowed</source>
+        <translation>Löschen ist nicht erlaubt</translation>
+    </message>
+</context>
+<context>
+    <name>EventPage</name>
+    <message>
+        <source>Details</source>
+        <translation>Details</translation>
+    </message>
+    <message>
+        <source>Incoming call</source>
+        <translation>Eingehender Anruf</translation>
+    </message>
+    <message>
+        <source>Outgoing call</source>
+        <translation>Ausgehender Anruf</translation>
+    </message>
+    <message>
+        <source>Partial call</source>
+        <translation>Partieller Anruf</translation>
+    </message>
+</context>
+<context>
+    <name>EventsDelegate</name>
+    <message>
+        <source>Armed for recording</source>
+        <translation>Aufnahmebereit</translation>
+    </message>
+    <message>
+        <source>Recording in progress</source>
+        <translation>Aufnahme läuft</translation>
+    </message>
+    <message>
+        <source>Recording suspended</source>
+        <translation>Aufnahme ausgesetzt</translation>
+    </message>
+</context>
+<context>
+    <name>EventsPage</name>
+    <message>
+        <source>Recordings</source>
+        <translation>Aufnahmen</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Löschen</translation>
+    </message>
+    <message>
+        <source>%1 is whitelisted</source>
+        <translation>%1 befindet sich in der weißen Liste</translation>
+    </message>
+    <message>
+        <source>%1 is blacklisted</source>
+        <translation>%1 befindet sich in der schwarzen Liste</translation>
+    </message>
+    <message>
+        <source>Always record this number</source>
+        <translation>Immer diese Nummer aufnehmen</translation>
+    </message>
+    <message>
+        <source>Always record %1</source>
+        <translation>Endlose Aufnahme %1</translation>
+    </message>
+    <message>
+        <source>Recording %1</source>
+        <translation>Aufnahme %1</translation>
+    </message>
+    <message>
+        <source>Never record this number</source>
+        <translation>Niemals diese Nummer aufnehmen</translation>
+    </message>
+    <message>
+        <source>Never record %1</source>
+        <translation>Keine Aufnahme %1</translation>
+    </message>
+    <message>
+        <source>Not recording %1</source>
+        <translation>Nicht aufgenommen %1</translation>
+    </message>
+    <message>
+        <source>Deleting</source>
+        <translation>Gelöscht</translation>
+    </message>
+    <message>
+        <source>No calls recorded yet</source>
+        <translation>Noch keine Anrufe aufgenommen</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <source>Select recordings</source>
+        <translation>Aufnahmen auswählen</translation>
+    </message>
+    <message>
+        <source>Enter phone number</source>
+        <translation>Telefonnummer eingeben</translation>
+    </message>
+</context>
+<context>
+    <name>EventsPicker</name>
+    <message>
+        <source>Select Recordings</source>
+        <translation>Aufnahmen auswählen</translation>
+    </message>
+    <message>
+        <source>No calls recorded yet</source>
+        <translation>Noch keine Anrufe aufgenommen</translation>
+    </message>
+    <message>
+        <source>Delete all</source>
+        <translation>Alle Löschen</translation>
+    </message>
+    <message>
+        <source>Deleting all recordings</source>
+        <translation>Alle Aufnahmen löschen</translation>
+    </message>
+    <message>
+        <source>Deleting recordings</source>
+        <translation>Aufnahmen löschen</translation>
+    </message>
+</context>
+<context>
+    <name>License</name>
+    <message>
+        <source>License</source>
+        <translation>Lizenz</translation>
+    </message>
+</context>
+<context>
+    <name>LocaleListModel</name>
+    <message>
+        <source>System</source>
+        <translation>System</translation>
+    </message>
+</context>
+<context>
+    <name>PhoneNumberEntryField</name>
+    <message>
+        <source>Search by name or number</source>
+        <translation>Suchen nach Name oder Nummer</translation>
+    </message>
+</context>
+<context>
+    <name>PhoneNumbersListDialog</name>
+    <message>
+        <source>Copy from white list</source>
+        <translation>Aus der weißen Liste kopieren</translation>
+    </message>
+    <message>
+        <source>Copy from black list</source>
+        <translation>Aus der schwarzen Liste kopieren</translation>
+    </message>
+    <message>
+        <source>Delete all</source>
+        <translation>Alle löschen</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Speichern</translation>
+    </message>
+    <message>
+        <source>Enter phone number</source>
+        <translation>Telefonnummer eingeben</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Löschen</translation>
+    </message>
+    <message>
+        <source>No items in the list</source>
+        <translation>Keine Einträge in der Liste</translation>
+    </message>
+    <message>
+        <source>Add numbers with field above or use pull-down menu</source>
+        <translation>Fügen Sie Nummern im Bereich oben oder verwenden Sie das Pulldown-Menü</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <source>Recording daemon</source>
+        <translation>Aufnahme-Daemon</translation>
+    </message>
+    <message>
+        <source>Storage</source>
+        <translation>Speicherung</translation>
+    </message>
+    <message>
+        <source>Audio settings</source>
+        <translation>Audio Einstellungen</translation>
+    </message>
+    <message>
+        <source>User interface</source>
+        <translation>Benutzeroberfläche</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Über</translation>
+    </message>
+</context>
+<context>
+    <name>Storage</name>
+    <message>
+        <source>Storage</source>
+        <translation>Speicherung</translation>
+    </message>
+    <message>
+        <source>Location</source>
+        <translation>Standort</translation>
+    </message>
+    <message>
+        <source>Location for storing the recordings</source>
+        <translation>Standort zum Speichern der Aufnahmen</translation>
+    </message>
+    <message>
+        <source>Browse</source>
+        <translation>Durchsuchen</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Speichern</translation>
+    </message>
+    <message>
+        <source>Relocating files</source>
+        <translation>Dateien verlagern</translation>
+    </message>
+</context>
+<context>
+    <name>UserInterface</name>
+    <message>
+        <source>User interface</source>
+        <translation>Benutzeroberfläche</translation>
+    </message>
+    <message>
+        <source>Locale</source>
+        <translation>Lokal</translation>
+    </message>
+    <message>
+        <source>Please restart the application if the locale is changed</source>
+        <translation>Bitte starten Sie die Anwendung neu, wenn das Gebietsschema geändert wird</translation>
+    </message>
+</context>
+</TS>
