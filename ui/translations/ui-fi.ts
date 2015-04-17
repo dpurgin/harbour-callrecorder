a9802a50dbe6d0dbeedba53dd4d86565e0791e87
Dmitriy Purgin <dpurgin@gmail.com>
2015-04-17 22:35:23 +0600

    [ui] Finnish translation

---
+++ ui/translations/ui-fi.ts
@@ -0,0 +1,399 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fi">
+<context>
+    <name>About</name>
+    <message>
+        <source>About</source>
+        <translation>Tietoja</translation>
+    </message>
+    <message>
+        <source>Call Recorder</source>
+        <translation>Puhelutallennus</translation>
+    </message>
+    <message>
+        <source>for SailfishOS</source>
+        <translation>SailfishOS:lle</translation>
+    </message>
+    <message>
+        <source>Copyright © 2014-2015 Dmitriy Purgin</source>
+        <translation>Tekijänoikeudet © 2014-2015 Dmitriy Purgin</translation>
+    </message>
+    <message>
+        <source>Thanks to Simonas Leleiva and Juho Hämäläinen</source>
+        <translation>Kiitos Simonas Leleiva ja Juho Hämäläinen</translation>
+    </message>
+    <message>
+        <source>This program comes with ABSOLUTELY NO WARRANTY. This is free software, and you are welcome to redistribute it under certain conditions; click on the button below for details</source>
+        <translation>Tämä ohjelma on lisensoitu ilmaiseksi ja tälle ohjelmalle ei myönnetä takuuta lain sallimissa rajoissa. Kuka tahansa voi muuttaa tai levittää ohjelmaa alla olevien GPL-lisenssiehtojen mukaisesti.</translation>
+    </message>
+    <message>
+        <source>Show license</source>
+        <translation>Näytä lisenssi</translation>
+    </message>
+    <message>
+        <source>Version %1</source>
+        <translation>Versio %1</translation>
+    </message>
+</context>
+<context>
+    <name>AudioSettings</name>
+    <message>
+        <source>Audio Settings</source>
+        <translation>Ääniasetukset</translation>
+    </message>
+    <message>
+        <source>FLAC Encoder</source>
+        <translation>FLAC muunnin</translation>
+    </message>
+    <message>
+        <source>Sample rate</source>
+        <translation>Näytteenottotaajuus</translation>
+    </message>
+    <message>
+        <source>FLAC compression level</source>
+        <translation>FLAC-pakkaustaso</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Call Recorder</source>
+        <translation>Puhelutallennus</translation>
+    </message>
+</context>
+<context>
+    <name>Daemon</name>
+    <message>
+        <source>Recording daemon</source>
+        <translation>Tallennuspalvelu</translation>
+    </message>
+    <message>
+        <source>Startup</source>
+        <translation>Käynnistäminen</translation>
+    </message>
+    <message>
+        <source>Active</source>
+        <translation>Aktiivinen</translation>
+    </message>
+    <message>
+        <source>Capture all incoming and outgoing calls</source>
+        <translation>Tallenna kaikki saapuvat ja lähteävät puhelut</translation>
+    </message>
+    <message>
+        <source>Automatic startup</source>
+        <translation>Automaatiinen käynnistäminen</translation>
+    </message>
+    <message>
+        <source>Start automatically upon reboot</source>
+        <translation>Käynnistä tallennus automaattisesti uudelleenkäynnistettäessä</translation>
+    </message>
+    <message>
+        <source>Operation mode</source>
+        <translation>Toimintatila</translation>
+    </message>
+    <message>
+        <source>Record calls to any number except those in the black list</source>
+        <translation>Tallentaa puhelut kaikista niistä numeroista jotka eivät ole estolistalla</translation>
+    </message>
+    <message>
+        <source>Do not record anything except numbers in the white list</source>
+        <translation>Tallentaa vain ne puhelut jotka ovat sallittujen listalla</translation>
+    </message>
+    <message>
+        <source>Black list</source>
+        <translation>Estolista</translation>
+    </message>
+    <message>
+        <source>White list</source>
+        <translation>Sallittujen lista</translation>
+    </message>
+    <message>
+        <source>Edit list</source>
+        <translation>Muokkaa listaa</translation>
+    </message>
+</context>
+<context>
+    <name>DirectoryNameDialog</name>
+    <message>
+        <source>Directory name</source>
+        <translation>Hakemisto</translation>
+    </message>
+</context>
+<context>
+    <name>DirectoryPickerDialog</name>
+    <message>
+        <source>Select %1</source>
+        <translation>Valitse %1</translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation>Valitse</translation>
+    </message>
+    <message>
+        <source>No nested directories</source>
+        <translation>Ei hakemistoja</translation>
+    </message>
+    <message>
+        <source>Rename</source>
+        <translation>Muokkaa nimeä</translation>
+    </message>
+    <message>
+        <source>Renaming is not allowed</source>
+        <translation>Nimeä ei voi muuttaa</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Poista</translation>
+    </message>
+    <message>
+        <source>Removal is not allowed</source>
+        <translation>Poistaminen ei ole sallittua</translation>
+    </message>
+</context>
+<context>
+    <name>EventPage</name>
+    <message>
+        <source>Details</source>
+        <translation>Lisätiedot</translation>
+    </message>
+    <message>
+        <source>Incoming call</source>
+        <translation>Saapunut puhelu</translation>
+    </message>
+    <message>
+        <source>Outgoing call</source>
+        <translation>Soitettu puhelu</translation>
+    </message>
+    <message>
+        <source>Partial call</source>
+        <translation>Osittainen puhelu</translation>
+    </message>
+</context>
+<context>
+    <name>EventsDelegate</name>
+    <message>
+        <source>Armed for recording</source>
+        <translation>Valmis tallentamaan</translation>
+    </message>
+    <message>
+        <source>Recording in progress</source>
+        <translation>Tallennus käynnissä</translation>
+    </message>
+    <message>
+        <source>Recording suspended</source>
+        <translation>Tallennus keskeytetty</translation>
+    </message>
+</context>
+<context>
+    <name>EventsPage</name>
+    <message>
+        <source>Recordings</source>
+        <translation>Tallennukset</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Poista</translation>
+    </message>
+    <message>
+        <source>%1 is whitelisted</source>
+        <translation>%1 on lisätty sallittujen listalle</translation>
+    </message>
+    <message>
+        <source>%1 is blacklisted</source>
+        <translation>%1 on lisätty estolistalle</translation>
+    </message>
+    <message>
+        <source>Always record this number</source>
+        <translation>Tallenna aina puhelut</translation>
+    </message>
+    <message>
+        <source>Always record %1</source>
+        <translation>Tallenna aina %1</translation>
+    </message>
+    <message>
+        <source>Recording %1</source>
+        <translation>Tallennetaan %1</translation>
+    </message>
+    <message>
+        <source>Never record this number</source>
+        <translation>Älä tallenna puheluita</translation>
+    </message>
+    <message>
+        <source>Never record %1</source>
+        <translation>Älä tallenna %1</translation>
+    </message>
+    <message>
+        <source>Not recording %1</source>
+        <translation>Ei tallenneta puheluita %1</translation>
+    </message>
+    <message>
+        <source>Deleting</source>
+        <translation>Poistetaan</translation>
+    </message>
+    <message>
+        <source>No calls recorded yet</source>
+        <translation>Ei tallennettuja puheluita</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Asetukset</translation>
+    </message>
+    <message>
+        <source>Select recordings</source>
+        <translation>Valitse tallennuksia</translation>
+    </message>
+    <message>
+        <source>Enter phone number</source>
+        <translation>Puhelinnumero</translation>
+    </message>
+</context>
+<context>
+    <name>EventsPicker</name>
+    <message>
+        <source>Select Recordings</source>
+        <translation>Valitse tallennuksia</translation>
+    </message>
+    <message>
+        <source>No calls recorded yet</source>
+        <translation>Ei tallennetttuja puheluita</translation>
+    </message>
+    <message>
+        <source>Delete all</source>
+        <translation>Poista kaikki</translation>
+    </message>
+    <message>
+        <source>Deleting all recordings</source>
+        <translation>Poistetaan kaikki tallennukset</translation>
+    </message>
+    <message>
+        <source>Deleting recordings</source>
+        <translation>Tallenuksia poistetaan</translation>
+    </message>
+</context>
+<context>
+    <name>License</name>
+    <message>
+        <source>License</source>
+        <translation>Lisenssi</translation>
+    </message>
+</context>
+<context>
+    <name>LocaleListModel</name>
+    <message>
+        <source>System</source>
+        <translation>Järjestelmä</translation>
+    </message>
+</context>
+<context>
+    <name>PhoneNumberEntryField</name>
+    <message>
+        <source>Search by name or number</source>
+        <translation>Etsi nimellä tai numerolla</translation>
+    </message>
+</context>
+<context>
+    <name>PhoneNumbersListDialog</name>
+    <message>
+        <source>Copy from white list</source>
+        <translation>Kopioi sallittujen listalta</translation>
+    </message>
+    <message>
+        <source>Copy from black list</source>
+        <translation>Kopioi estolistalta</translation>
+    </message>
+    <message>
+        <source>Delete all</source>
+        <translation>Poista kaikki</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Tallenna</translation>
+    </message>
+    <message>
+        <source>Enter phone number</source>
+        <translation>Puhelinnumero</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Poista</translation>
+    </message>
+    <message>
+        <source>No items in the list</source>
+        <translation>Ei valittuja kohteita</translation>
+    </message>
+    <message>
+        <source>Add numbers with field above or use pull-down menu</source>
+        <translation>Lisää numeroita ylläolevaa kenttää tai alasvetovalikkoa käyttämällä</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Asetukset</translation>
+    </message>
+    <message>
+        <source>Recording daemon</source>
+        <translation>Tallennuspalvelu</translation>
+    </message>
+    <message>
+        <source>Storage</source>
+        <translation>Tallenustila</translation>
+    </message>
+    <message>
+        <source>Audio settings</source>
+        <translation>Ääniasetukset</translation>
+    </message>
+    <message>
+        <source>User interface</source>
+        <translation>Käyttöliittymä</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Tietoja</translation>
+    </message>
+</context>
+<context>
+    <name>Storage</name>
+    <message>
+        <source>Storage</source>
+        <translation>Tallennustila</translation>
+    </message>
+    <message>
+        <source>Location</source>
+        <translation>Tiedostopolku</translation>
+    </message>
+    <message>
+        <source>Location for storing the recordings</source>
+        <translation>Tiedostopolku johon puheluäänitykset tallennetaan</translation>
+    </message>
+    <message>
+        <source>Browse</source>
+        <translation>Selaa</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Tallenna</translation>
+    </message>
+    <message>
+        <source>Relocating files</source>
+        <translation>Siirretään tiedostoja</translation>
+    </message>
+</context>
+<context>
+    <name>UserInterface</name>
+    <message>
+        <source>User interface</source>
+        <translation>Käyttöliittymä</translation>
+    </message>
+    <message>
+        <source>Locale</source>
+        <translation>Kielialue</translation>
+    </message>
+    <message>
+        <source>Please restart the application if the locale is changed</source>
+        <translation>Käynnistä sovellus uudelleen jos kielialue muuttuu</translation>
+    </message>
+</context>
+</TS>
