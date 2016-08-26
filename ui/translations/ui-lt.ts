fca40262291e2e3ee1726a9344f98c1704011a0c
Dmitriy Purgin <dpurgin@gmail.com>
2016-08-26 23:16:20 +0200

    [ui] Lithuanian translation added

---
+++ ui/translations/ui-lt.ts
@@ -0,0 +1,647 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="lt" version="2.1">
+<context>
+    <name>About</name>
+    <message>
+        <source>About</source>
+        <translation>Apie</translation>
+    </message>
+    <message>
+        <source>Call Recorder</source>
+        <translation>Skambučių įrašymo programa</translation>
+    </message>
+    <message>
+        <source>for SailfishOS</source>
+        <translation>Skirta SailfishOS</translation>
+    </message>
+    <message>
+        <source>Copyright © 2014-2015 Dmitriy Purgin</source>
+        <translation>Autorinės teisės   © 2014-2015 Dmitriy Purgin</translation>
+    </message>
+    <message>
+        <source>Thanks to Simonas Leleiva and Juho Hämäläinen</source>
+        <translation>Padėka Simonas Leleiva ir Juho Hämäläinen</translation>
+    </message>
+    <message>
+        <source>Version %1</source>
+        <translation>Versija %1</translation>
+    </message>
+    <message>
+        <source>Translators</source>
+        <translation>Vertėjai</translation>
+    </message>
+    <message>
+        <source>License</source>
+        <translation>Licenzija</translation>
+    </message>
+    <message>
+        <source>Use pull-down menu to see translators</source>
+        <translation>
+Norint pamatyt vertimus, naudokite iškleidžiamą meniu</translation>
+    </message>
+    <message>
+        <source>This program comes with ABSOLUTELY NO WARRANTY. This is free software, and you are welcome to redistribute it under certain conditions; use pull-down menu for details</source>
+        <translation>Ši programa yra be garantijos.Tai nemokama programinė įranga, ją galite laisvai naudoti, spausti iškleidžiamą meniu mygtuką jei nori sužinoti daugiau</translation>
+    </message>
+</context>
+<context>
+    <name>AudioSettings</name>
+    <message>
+        <source>Audio Settings</source>
+        <translation>Garso nustatymai</translation>
+    </message>
+    <message>
+        <source>FLAC Encoder</source>
+        <translation>Flac </translation>
+    </message>
+    <message>
+        <source>Sample rate</source>
+        <translation>Dažnis</translation>
+    </message>
+    <message>
+        <source>FLAC compression level</source>
+        <translation>FLAC suspaudimo lygis</translation>
+    </message>
+    <message>
+        <source>44.1 kHz</source>
+        <translation>44.1 kHz</translation>
+    </message>
+    <message>
+        <source>32 kHz</source>
+        <translation>32 kHz</translation>
+    </message>
+    <message>
+        <source>22.05 kHz</source>
+        <translation>22.05 kHz</translation>
+    </message>
+    <message>
+        <source>16 kHz</source>
+        <translation>16 kHz</translation>
+    </message>
+    <message>
+        <source>11.025 kHz</source>
+        <translation>11.025 kHz</translation>
+    </message>
+    <message>
+        <source>8 kHz</source>
+        <translation>8 kHz</translation>
+    </message>
+</context>
+<context>
+    <name>ContactPickerDialog</name>
+    <message>
+        <source>Accept</source>
+        <translation>Priimti</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>atšaukti</translation>
+    </message>
+    <message>
+        <source>No contacts available</source>
+        <translation>Nėra kontaktų</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Call Recorder</source>
+        <translation>Skambučių registras</translation>
+    </message>
+</context>
+<context>
+    <name>Daemon</name>
+    <message>
+        <source>Recording daemon</source>
+        <translation>Įrašoma deamon</translation>
+    </message>
+    <message>
+        <source>Startup</source>
+        <translation>Pradėti</translation>
+    </message>
+    <message>
+        <source>Active</source>
+        <translation>Aktyvus</translation>
+    </message>
+    <message>
+        <source>Capture all incoming and outgoing calls</source>
+        <translation>Fiksuoti visus įeinančius ir išeinančius skambučius</translation>
+    </message>
+    <message>
+        <source>Automatic startup</source>
+        <translation>Automatinis paleidimas</translation>
+    </message>
+    <message>
+        <source>Start automatically upon reboot</source>
+        <translation>Pradėti automatiškai, persikrovus</translation>
+    </message>
+    <message>
+        <source>Operation mode</source>
+        <translation>Darbo režimas</translation>
+    </message>
+    <message>
+        <source>Record calls to any number except those in the black list</source>
+        <translation>Įrašų skambučiai į bet kokį skaičių išskyrus į juodąjį sąrašą</translation>
+    </message>
+    <message>
+        <source>Do not record anything except numbers in the white list</source>
+        <translation>Negalima įrašyti nieko, išskyrus skaičių baltąjį sąrašą</translation>
+    </message>
+    <message>
+        <source>Black list</source>
+        <translation>Juodas sąrašas</translation>
+    </message>
+    <message>
+        <source>White list</source>
+        <translation>baltasis sąrašas
+</translation>
+    </message>
+    <message>
+        <source>Edit list</source>
+        <translation>Redaguoti sarašą</translation>
+    </message>
+</context>
+<context>
+    <name>DirectoryNameDialog</name>
+    <message>
+        <source>Directory name</source>
+        <translation>Katalogo vardas</translation>
+    </message>
+    <message>
+        <source>Accept</source>
+        <translation>Priimti</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Atšaukti</translation>
+    </message>
+</context>
+<context>
+    <name>DirectoryPickerDialog</name>
+    <message>
+        <source>Select %1</source>
+        <translation>Pasirinkti %1</translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation>Pasirinkti</translation>
+    </message>
+    <message>
+        <source>No nested directories</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Rename</source>
+        <translation>Pervandinti</translation>
+    </message>
+    <message>
+        <source>Renaming is not allowed</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Ištrinti</translation>
+    </message>
+    <message>
+        <source>Removal is not allowed</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Atšaukti</translation>
+    </message>
+</context>
+<context>
+    <name>EventPage</name>
+    <message>
+        <source>Details</source>
+        <translation>Detalės</translation>
+    </message>
+    <message>
+        <source>Incoming call</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Outgoing call</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Partial call</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Ištrinti</translation>
+    </message>
+    <message>
+        <source>Approve and store</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Number is whitelisted</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Number is blacklisted</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Always record this number</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Never record this number</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Recording %1</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Not recording %1</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Storing</source>
+        <translation>Saugoma</translation>
+    </message>
+    <message>
+        <source>Deleting</source>
+        <translation>Ištrinama</translation>
+    </message>
+</context>
+<context>
+    <name>EventsDelegate</name>
+    <message>
+        <source>Deleting</source>
+        <translation>Ištrinama</translation>
+    </message>
+    <message>
+        <source>Storing</source>
+        <translation>Saugoma</translation>
+    </message>
+</context>
+<context>
+    <name>EventsDelegateContent</name>
+    <message>
+        <source>Armed for recording</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Recording in progress</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Recording suspended</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Waiting for approval</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>EventsDelegateMenu</name>
+    <message>
+        <source>Delete</source>
+        <translation>Ištrinti</translation>
+    </message>
+    <message>
+        <source>Always record this number</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Recording %1</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Never record this number</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Not recording %1</source>
+        <translation>Neįrašyti šito numerio</translation>
+    </message>
+    <message>
+        <source>Approve and store</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Number is whitelisted</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Number is blacklisted</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>EventsFilterDialog</name>
+    <message>
+        <source>Accept</source>
+        <translation>Priimti</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Atšaukti</translation>
+    </message>
+    <message>
+        <source>Before</source>
+        <translation>Prieš</translation>
+    </message>
+    <message>
+        <source>After</source>
+        <translation>Po</translation>
+    </message>
+    <message>
+        <source>Phone number</source>
+        <translation>Telefono numeris</translation>
+    </message>
+    <message>
+        <source>On</source>
+        <translation>įjungta</translation>
+    </message>
+</context>
+<context>
+    <name>EventsPage</name>
+    <message>
+        <source>Recordings</source>
+        <translation>Įrašai</translation>
+    </message>
+    <message>
+        <source>Select recordings</source>
+        <translation>Pasirinkti įrašus</translation>
+    </message>
+    <message>
+        <source>Filtered recordings</source>
+        <translation>Filtruoti įrašai</translation>
+    </message>
+    <message>
+        <source>Select filtered recordings</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Nustatymai</translation>
+    </message>
+    <message>
+        <source>Filter</source>
+        <translation>Filtras</translation>
+    </message>
+    <message>
+        <source>No recordings meet filter criteria</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>No calls recorded yet</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>View recordings</source>
+        <translation>Žiurėti įrašus</translation>
+    </message>
+    <message>
+        <source>Deleting recordings</source>
+        <translation>Ištrinti įrašus</translation>
+    </message>
+    <message>
+        <source>Delete all filtered</source>
+        <translation>Ištrinti visus filtruotus</translation>
+    </message>
+    <message>
+        <source>Delete all</source>
+        <translation>Ištrinti visus </translation>
+    </message>
+    <message>
+        <source>Deleting all filtered</source>
+        <translation>Ištrinti visus filtruotus</translation>
+    </message>
+    <message>
+        <source>Deleting all recordings</source>
+        <translation>Ištrinti įrašus</translation>
+    </message>
+</context>
+<context>
+    <name>License</name>
+    <message>
+        <source>License</source>
+        <translation>Licenzija</translation>
+    </message>
+</context>
+<context>
+    <name>LocaleListModel</name>
+    <message>
+        <source>System</source>
+        <translation>Sistema</translation>
+    </message>
+</context>
+<context>
+    <name>PhoneNumberEntryField</name>
+    <message>
+        <source>Search by name or number</source>
+        <translation>Ieškoti pagal numerį</translation>
+    </message>
+</context>
+<context>
+    <name>PhoneNumbersListDialog</name>
+    <message>
+        <source>Copy from white list</source>
+        <translation>Kopijuoti iš baltojo sąrašo</translation>
+    </message>
+    <message>
+        <source>Copy from black list</source>
+        <translation>Kopijuoti iš juodojo sąrašo</translation>
+    </message>
+    <message>
+        <source>Delete all</source>
+        <translation>Ištrinti visus </translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Išsaugoti</translation>
+    </message>
+    <message>
+        <source>Enter phone number</source>
+        <translation>įvesti telefono numerį</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Ištrinti</translation>
+    </message>
+    <message>
+        <source>No items in the list</source>
+        <translation>Tuščias sąrašas</translation>
+    </message>
+    <message>
+        <source>Add numbers with field above or use pull-down menu</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Atšaukti</translation>
+    </message>
+    <message>
+        <source>Pick from contacts</source>
+        <translation>Išsirinkti iš kontakto</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Nustatymai</translation>
+    </message>
+    <message>
+        <source>Recording daemon</source>
+        <translation>Įrašomas deamon</translation>
+    </message>
+    <message>
+        <source>Storage</source>
+        <translation>Saugykla</translation>
+    </message>
+    <message>
+        <source>Audio settings</source>
+        <translation>Audio nustatymai</translation>
+    </message>
+    <message>
+        <source>User interface</source>
+        <translation>Vartotojo sąsaja</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Apie</translation>
+    </message>
+</context>
+<context>
+    <name>Storage</name>
+    <message>
+        <source>Storage</source>
+        <translation>Saugykla</translation>
+    </message>
+    <message>
+        <source>Location</source>
+        <translation>Vieta</translation>
+    </message>
+    <message>
+        <source>Location for storing the recordings</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Browse</source>
+        <translation>Ieškoti</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Išsaugoti</translation>
+    </message>
+    <message>
+        <source>Relocating files</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Storage limits</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Limit storage by size or age</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>By age</source>
+        <translation>Pagal amžių</translation>
+    </message>
+    <message>
+        <source>no limit</source>
+        <translation>Neribotas</translation>
+    </message>
+    <message>
+        <source>30 days</source>
+        <translation>30 dienų</translation>
+    </message>
+    <message>
+        <source>90 days</source>
+        <translation>90 dienų</translation>
+    </message>
+    <message>
+        <source>180 days</source>
+        <translation>180 dienų</translation>
+    </message>
+    <message>
+        <source>365 days</source>
+        <translation>365 dienos</translation>
+    </message>
+    <message>
+        <source>Custom age limit in days</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>By size</source>
+        <translation>Pagal dydį</translation>
+    </message>
+    <message>
+        <source>300 MB</source>
+        <translation>300 MB</translation>
+    </message>
+    <message>
+        <source>500 MB</source>
+        <translation>500 MB</translation>
+    </message>
+    <message>
+        <source>1 GB</source>
+        <translation>1 GB</translation>
+    </message>
+    <message>
+        <source>3 GB</source>
+        <translation>3 GB</translation>
+    </message>
+    <message>
+        <source>5 GB</source>
+        <translation>5 GB</translation>
+    </message>
+    <message>
+        <source>Custom size limit in MB</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Approval for storage</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Require approval</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>If checked, an approval of storage will be shown after each recorded call</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>custom</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>Translators</name>
+    <message>
+        <source>Translators</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Thanks to these people the Call Recorder is available in different languages. Names and locale codes are listed in alphabetical order.</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>UserInterface</name>
+    <message>
+        <source>User interface</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Locale</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Please restart the application if the locale is changed</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+</TS>
